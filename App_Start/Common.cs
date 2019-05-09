8fc9c55f829c2724aa251abf9fb956b0ed00dd97
MDev0x <mahmotoncor@gmail.com>
2019-05-09 22:15:05 +0300

    Initial commit

---
+++ App_Start/Common.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc5mongocrud.App_Start
+{
+    public class Common
+    {
+        public class Base
+        {
+            public Base()
+            {
+                this._id = Guid.NewGuid().ToString();
+            }
+            [BsonElement("_id")]
+            public string _id { get; set; }
+        }
+
+        public static IMongoDatabase conn;
+        public static IMongoDatabase Connector
+        {
+            get
+            {
+                if (conn == null)
+                {
+                    MongoClient client = new MongoClient("mongodb://localhost:27017");
+                    conn = client.GetDatabase("aful");
+                    return conn;
+                }
+                return conn;
+            }
+            set { }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# MVC5 - Simple CRUD Operations with MongoDB
+This repository create with using **.NET Framework 4.7.2** and **MongoDB.Driver** NuGet package
+
+Methods | Route | Result
+--------|-------|-------
+GET | /api/v1/users | User model as JSON from MongoDB
+GET | /api/v1/users/:id | User model by id as JSON from MongoDB
+POST | /api/v1/users | Add user model to MongoDB
+PUT | /api/v1/users/:id | Update user model by id from MongoDB
+DELETE | /api/v1/users/:id | Delete user model by id from MongoDB
+
+### This result returned when model updated
+![alt-text](https://s.put.re/a3fMUr14.png)
